6e8d5ab69c54de54fe003163b8d6616e05bfffc5
Ihya Fauzi <mioscape@Ihyas-MacBook-Pro.local>
2023-10-17 16:07:06 +0700

    first commit

---
+++ lib/baris_kolom.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class BarisKolom extends StatelessWidget {
+  const BarisKolom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Widget Baris dan Kolom"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CellWidget('Baris 1, Kolom 1'),
+              CellWidget('Baris 1, Kolom 2'),
+              CellWidget('Baris 1, Kolom 3'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CellWidget('Baris 2, Kolom 1'),
+              CellWidget('Baris 2, Kolom 2'),
+              CellWidget('Baris 2, Kolom 3'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CellWidget('Baris 3, Kolom 1'),
+              CellWidget('Baris 3, Kolom 2'),
+              CellWidget('Baris 3, Kolom 3'),
+            ],
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class CellWidget extends StatelessWidget {
+  final String text;
+
+  CellWidget(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(text, style: TextStyle(fontSize: 16.0)),
+    );
+  }
+}
